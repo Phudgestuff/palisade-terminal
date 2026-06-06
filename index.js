c2f7fac02ad91e88f260c9de25034e82bb139488
Hugo Bell <bellhugo30@gmail.com>
2026-06-06 16:16:00 +0100

    Fix data reading from json

--- index.js
+++ index.js
@@ -1,39 +1,22 @@
-// get the data from the information.json file
-let data;
-async function fetchJSONData() {
-    try {
-        const response = await fetch('information.json');
-        if (!response.ok) {
-            throw new Error();
-        }
-
-        data = await response.json();
-    } catch (error) {
-        console.error("unable to fetch data")
-    }
-}
-fetchJSONData();
-
 // make sure the input box is always focused on
-let inputSpace = document.getElementById("input");
+const inputSpace = document.getElementById("input");
 inputSpace.addEventListener('blur', () => {
     inputSpace.focus();
 });
 
 // create scanlines for CRT effect
-let scanlinesDiv = document.getElementById("scanlines");
-let height = window.innerHeight;
-let scanLineSpace = 15; 
+const scanlinesDiv = document.getElementById("scanlines");
+const height = globalThis.innerHeight;
+const scanLineSpace = 15; 
 for (let i = 0; i < height-scanLineSpace; i += scanLineSpace) {
-    let el = document.createElement("div");
+    const el = document.createElement("div");
     el.className = "scanline";
     el.style.top = `${i}px`;
     scanlinesDiv.appendChild(el);
 }
 
 // add text to the terminal output part by part
-let targetDiv = document.getElementById("output");
-let text = data["introtext"];
+const targetDiv = document.getElementById("output");
 function startTypeWriter(textToType) {
     let index = 0;
     const typingSpeed = 20; // Delay between characters in milliseconds
@@ -57,7 +40,24 @@ function startTypeWriter(textToType) {
 
     typeWriter();
 }
+
+// get the data from the information.json file
+async function fetchJSONData() {
+    try {
+        const response = await fetch('information.json');
+        if (!response.ok) {
+            throw new Error();
+        }
+
+        let data = await response.json();
+
+        startTypeWriter(data["introtext"]);
+    } catch (error) {
+        console.error("unable to fetch data")
+    }
+}
+fetchJSONData();
 new Promise((resolve, reject) => startTypeWriter(text));
 
-let input = document.getElementById("input");
+const input = document.getElementById("input");
 
